b7b7150b072ef76c8317ec69e2f226f2ef9d5f16
Jonathan Miller <jonathan_miller@Jonathans-MacBook-Air.local>
2020-12-20 17:36:26 +0000

    repository_added_and_set_up

---
+++ controllers/vet_controllers.py
@@ -0,0 +1,5 @@
+from Flask import Flask, render_template, request, redirect
+from Flask import blueprint
+
+from models.animal import Animal
+from models.vet import Vet
---
+++ repositories/vet_repository.py
@@ -0,0 +1,6 @@
+from db.run_sql import run_sql
+
+from models.animal import Animal
+from models.vet import Vet
+
+from repositories import animal_repository
