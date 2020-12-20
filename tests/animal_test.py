5b1b49ad1b8f4abe9e5bae5e17873c32c8907676
Jonathan Miller <jonathan_miller@Jonathans-MacBook-Air.local>
2020-12-20 13:20:57 +0000

    test_animal_name_success

--- tests/animal_test.py
+++ tests/animal_test.py
@@ -7,3 +7,6 @@ class TestAnimal(unittest.TestCase):
 
     def setUp(self):
         self.animal_1 = Animal("Dougal Miller", "14th August 1996", "Dog", "Susan Boyle", "Broken Leg")
+
+    def test_animal_name(self):
+        self.assertEqual("Dougal Miller", self.animal_1.name)
