ca0461c98fa356cf8bf432f7fa4fc0c87f1215f8
Jonathan Miller <jonathan_miller@Jonathans-MacBook-Air.local>
2020-12-20 13:02:44 +0000

    first_push

---
+++ class/animal.py
@@ -0,0 +1,11 @@
+class Animal:
+
+    def __init__(self, name, date_of_birth, type, owner_info, treatment_notes):
+        
+        self.name = name
+        self.date_of_birth = date_of_birth
+        self.type = type
+        self.owner_info = owner_info
+        self.treatment_notes = treatment_notes
+
+
